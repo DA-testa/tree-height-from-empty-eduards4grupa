17c569f6cd05486b686ef7d789ca07374cdbc1b1
eduards4grupa <125820885+eduards4grupa@users.noreply.github.com>
2023-03-06 01:35:49 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -9,8 +9,10 @@ def compute_height(n, parents):
     # Write this function
     children =[[] for _ in range(n)]
     for i in range(n):
-        if parents[i]!=-1:
+        if parents[i]!==-1:
             children[parents[i]].append(i)
+            
+            
     def height(node):
         if not children[node]:
             return 1
@@ -18,7 +20,7 @@ def compute_height(n, parents):
         else:
             #max_height=max(height(child) for child in children[node])
             return 1+max(height(child) for child in children[node])
-        root=parents.index(-1)
+    root=parents.index(-1)
     return height(root)
         
     # Your code here
@@ -39,10 +41,10 @@ def main():
         if 'a' in file_name:
             print("file name cant contain a letter")
             exit()
-        with open (file_name, 'r') as file:
+        with open ("text/"+file_name, 'r') as file:
             n=int(file.readline())
             parents=list(map(int, file.readline().split()))
-        print(cumpute_height(n, parents))
+            print(cumpute_height(n, parents))
     else:
         print("You have to write K or F!")
         exit()
@@ -52,12 +54,12 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-print(compute_height(n, parents))
+
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
+#main()
 # print(numpy.array([1,2,3]))
